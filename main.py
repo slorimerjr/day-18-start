b05bead4d7edf3976332ba9340f02208933e491b
slorimerjr <slorimerjr@gmail.com>
2022-08-17 12:56:04 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("VioletRed4")
+
+for _ in range(4):
+    tim.forward(100)
+    tim.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
