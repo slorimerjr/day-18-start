96d7533e5855114154d2738d398dce3851cbb87c
slorimerjr <slorimerjr@gmail.com>
2022-08-18 13:09:51 -0500

    simplified code finished - turtle draws triangle, square, pentagon, hexagon, heptagon, octagon, nonagon, decagon

--- main.py
+++ main.py
@@ -7,71 +7,25 @@ tim = t.Turtle()
 tim.shape("turtle")
 tim.color("VioletRed4")
 
-tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-for _ in range(3):
-    tim.forward(100)
-    tim.right(120)
+# 360 / num_angle = angle
 
-tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-for _ in range(4):
-    tim.forward(100)
-    tim.right(90)
-
-tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-for _ in range(5):
-    tim.forward(100)
-    tim.right(72)
-
-tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-for _ in range(6):
-    tim.forward(100)
-    tim.right(60)
-
-tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-for _ in range(7):
-    tim.forward(100)
-    tim.right(51.43)
-
-tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-for _ in range(8):
-    tim.forward(100)
-    tim.right(45)
-
-tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-for _ in range(9):
-    tim.forward(100)
-    tim.right(40)
-
-tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-for _ in range(10):
-    tim.forward(100)
-    tim.right(36)
+num = 3
+while num < 11:
+    tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
+    angle = 360 / num
+    for _ in range(num):
+        tim.forward(100)
+        tim.right(angle)
+    num += 1
 
 
-# teacher solution
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# # teacher's solution
+#
+# import turtle as t
+#
+# tim = t.Turtle()
+#
+#
 
 
 
