0ec56b57d1b25008e82a8a0d09c3daa9dfeff294
slorimerjr <slorimerjr@gmail.com>
2022-08-23 10:29:36 -0500

    completed random walk challenge

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import turtle as t
 from turtle import Turtle, Screen
+import random
 from random import randint
+
 
 t.colormode(255)
 tim = t.Turtle()
-tim.shape("turtle")
-tim.color("VioletRed4")
+tim.pensize(5)
 
 # 360 / num_angle = angle
 
-num = 3
-while num < 11:
-    tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-    angle = 360 / num
-    for _ in range(num):
-        tim.forward(100)
-        tim.right(angle)
-    num += 1
+# num = 3
+# while num < 11:
+#     tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
+#     angle = 360 / num
+#     for _ in range(num):
+#         tim.forward(100)
+#         tim.right(angle)
+#     num += 1
 
 
 # # teacher's solution
@@ -28,7 +29,7 @@ while num < 11:
 # tim = t.Turtle()
 #
 # num_sides = 5
-# colors = ["LightSteelBlue", "PaleGoldenrod", "Chartreuse", "DeepPink", "AntiqueWhite", "Peru", "Indigo", "DarkGray", "AliceBlue", "DarkSlateGray", "DarkRed"]
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 #
 # def draw_shape(num_sides):
 #     angle = 360 / num_sides
@@ -40,13 +41,25 @@ while num < 11:
 #     tim.color(random.choice(colors))
 #     draw_shape(shape_sides)
 
+# random walk solution
+# forward(), back(), right(), left()
+# tim.speed('slowest')
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.left(45)
+# tim.back(100)
 
+directions = [0, 90, 180, 270]
+tim.speed(10)
+tim.pensize(15)
 
-
-
-
-
-
+for _ in range(100):
+    tim.color(random.choice(colors))
+    tim.left(random.choice(directions))
+    tim.forward(25)
 
 screen = Screen()
 screen.exitonclick()
